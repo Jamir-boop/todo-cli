bfcb316b6b762ca2f16123c74cbf0839958ed572
Percy Brayam Cubas Muñoz <percybrayan18@gmail.com>
2022-06-04 16:44:14 -0500

    modificaciones del brayam

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from prompt_toolkit import PromptSession
 
 TODO = "todo.json"
 
+###########################################
+import json
+from pydoc import describe
+
+TODO = open('todo.json')
+DATA = json.load(TODO)
+
+
+
 # historial persistente
 session = PromptSession(history=FileHistory('.todo_history'))
 
@@ -10,6 +19,26 @@ session = PromptSession(history=FileHistory('.todo_history'))
 while True:
     text1 = session.prompt('> ')
     if text1.lower() == "list":
-        print(DATA)
+
+
+        # tasks = []
+
+        # class task:
+        #     def __init__(self, name, description, state):
+        #         self.name = name
+        #         self.description = description
+        #         self.state = state
+
+
+        for data in DATA["tasks"]:
+            #name = data["id"]
+            #description = data["description"]
+            #state = data["state"]
+            #tasks.append(task(name, description, state))
+
+            print("Task:",data["id"],"\n",
+                "\t", data["description"],"\n",
+                "\t", data["state"],"\n")
+      
     if text1 == "q" or text1 == "exit" or text1 == "quit":
         break
