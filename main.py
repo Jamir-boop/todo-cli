f1ead50ac7828e47001c215e42680f1c56a9184c
Percy Brayam Cubas Muñoz <percybrayan18@gmail.com>
2022-06-04 17:38:49 -0500

    modificaciones del brayam

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ast import Delete, Return
 from cgitb import text
 import json
 from prompt_toolkit.history import FileHistory
@@ -16,40 +17,39 @@ DATA = json.load(TODO)
 clear()
 
 
+def list(DATA):
+    for data in DATA["tasks"]:
+        print("Task:",data["id"],"\n",
+            "\t", data["description"],"\n",
+            "\t", data["state"],"\n")
+
+
+def delete(DATA, data_id):
+    for data in DATA["tasks"]:
+        if data["id"] == data_id:
+            DATA["tasks"].remove(data)
+
+
 
 # historial persistente
 session = PromptSession(history=FileHistory('.todo_history'))
 
 while True:
-    input = session.prompt('> ')
-    if input.lower() == "list":
+    input = session.prompt('> ').lower().split()
 
+    if input[0] == "list":
+        list(DATA)
 
-        # tasks = []
-
-        # class task:
-        #     def __init__(self, name, description, state):
-        #         self.name = name
-        #         self.description = description
-        #         self.state = state
-
-        
-
-
-        for data in DATA["tasks"]:
-            #name = data["id"]
-            #description = data["description"]
-            #state = data["state"]
-            #tasks.append(task(name, description, state))
-
-            print("Task:",data["id"],"\n",
-                "\t", data["description"],"\n",
-                "\t", data["state"],"\n")
-
-    if input.lower() == "clear":
+    if input[0] == "clear":
         clear()
-        
-      
-    if input == "q" or input == "exit" or input == "quit":
+    
+    if input[0] == "del":
+        try:
+            delete(DATA, input[1])
+            list(DATA)
+        except:
+            pass
+              
+    if input[0] == "q" or input[0] == "exit" or input[0] == "quit":
         clear()
         break
