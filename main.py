9ae12f31146d291e09ea87bc564862a2c65dfb90
Jamir <jeisereljamir@gmail.com>
2022-06-09 03:03:20 -0500

    update agregado

--- main.py
+++ main.py
@@ -11,24 +11,12 @@ from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 
 TODO = "todo.json"
-def reset_todo(): # para tests
+def reset_todo():
     DATA_INICIAL = {"tasks":[{"id":12,"priority":1,"description":"Primer pendiente y por eso el mejor","time":"2022-06-04 20:29:28.294142","state":0},{"id":13,"priority":0,"description":"crear clase de validator","time":"2022-06-04 20:29:28.294142","state":0},{"id":22,"priority":2,"description":"organizar clases","time":"2022-06-04 20:29:28.294142","state":1},{"id":40,"priority":2,"description":"este es un documento","time":"2022-06-08 23:29:49.074971","state":0}]}
     with open(TODO, 'w') as file:
         file.seek(0)
         json.dump(DATA_INICIAL, file, indent=4)
-reset_todo()
-
-def list(): # necesita agregar opciones de formateo ("compact", "detailed")
-    clear()
-    file = open('todo.json', "r+")
-    DATA = json.load(file)
-    contador = 0
-    for item in DATA["tasks"]:
-        contador += 1
-        if item["state"] == 1:
-            print(contador, "√", item["description"], item["priority"], "\n")
-        else:
-            print(contador, "•", item["description"], "\n")
+reset_todo() # para tests (resetea contenido de todo.json)
 
 def load_todo():
     with open(TODO) as file:
@@ -38,35 +26,50 @@ def save_todo(data):
     with open(TODO, 'w') as file:
         json.dump(data, file, indent=4)
 
+def list(): # necesita agregar opciones de formateo ("compact", "detailed")
+    clear()
+    DATA = load_todo()
+    contador = 0
+    for item in DATA["tasks"]:
+        contador += 1
+        if item["state"] == 1:
+            print(contador, "√", item["description"], "\n")
+        else:
+            print(contador, "•", item["description"], "\n")
+
 def delete(id):
     DATA = load_todo()
-    del DATA["tasks"][id-1]
+    del DATA["tasks"][int(id)-1]
     save_todo(DATA)
-
+    list()
 
 def create(input):
-    with open(TODO, "r+") as file:
-        description_index = input.index("add")
+    description_index = input.index("add")
+    last_index = len(input)
 
-        last_index = len(input)
-        description = ' '.join(input[description_index + 1:last_index])
+    description = ' '.join(input[description_index + 1:last_index])
 
-        DATA = json.load(file)
-        last_id = int(DATA["tasks"][-1]["id"])
+    DATA = load_todo()
+    last_id = int(DATA["tasks"][-1]["id"] + 1) # toma ultimo id + 1
 
-        DATA["tasks"].append(
-            {
-                "id": str(last_id + 1),
-                "description": description,
-                "state": 0,
-                "time": f"{datetime.now()}"
-            }
-        )
+    DATA["tasks"].append(
+        {
+            "id": last_id,
+            "priority": 2,
+            "description": description,
+            "state": 0,
+            "time": f"{datetime.now()}"
+        }
+    )
+    save_todo(DATA)
+    list()
 
-        file.seek(0)
-        json.dump(DATA, file, indent=4)
-        file.close()
-
+def update(id, input):
+    DATA = load_todo()
+    description = ' '.join(input)
+    DATA["tasks"][int(id)]["description"] = description
+    save_todo(DATA)
+    list()
 
 # historial persistente
 session = PromptSession(history=FileHistory('.todo_history'))
@@ -77,7 +80,7 @@ class StringValidator(Validator):
     def validate(self, document):
         text = document.text
         
-        if text and not text.isdigit():
+        if text and not text.isdigit():  # revisar (mal implementado)
             input = utils.parse(text)[0]
 
             if len(input) != 0:
@@ -85,13 +88,12 @@ class StringValidator(Validator):
                     list()        
                 if input[0] == "clear" or input[0] == "cls":
                     clear()
-                if input[0] == "add":
+                if input[0] == "add" or input[0] == "create":
                     create(input)
-                    list()
-                if input[0] == "del":
-                    delete(int(input[1]))
-                    list()
-
+                if input[0] == "del": # manejar input inadecuado (ej. fuera de index)
+                    delete(input[1])
+                if input[0] == "update" or input[0] == "up": # manejar input inadecuado (ej. fuera de index)
+                    update(input[1], input[2:])
                 if input[0] == "q" or input[0] == "exit" or input[0] == "quit":
                     exit()
         else:
