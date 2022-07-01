bcccdbbcc018958bfe10fe18039f98cc6c3f7a4e
jamir-boop <jeisereljamir@gmail.com>
2022-06-30 19:44:37 -0700

    create/add new task added

--- commands/create.py
+++ commands/create.py
@@ -18,18 +18,25 @@ class Create(Command):
     def do_command(self, *args):
         if args:
             DATA = self.load_todo()
+
             last_id = int(DATA["tasks"][-1]["id"])  # toma ultimo del grupo
             last_id += 1
+
+            description = ' '.join(args)
             DATA["tasks"].append(
                 {
                     "id": last_id,
                     "priority": 2,
-                    "description": args[1:],
+                    "description": description,
                     "state": 0,
                     "time": f"{datetime.datetime.now()}"
                 }
             )
             self.save_todo(DATA)
+
+            list_open = self.todo.commands.get("list")
+            list_open.do_command("list")
+
             self.todo.rprompt_message = f"{self.todo.time_emoji} Task {last_id} saved."
         else:
             self.todo.rprompt_message = f"{self.todo.time_emoji} Task has no content"
